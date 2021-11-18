754c54f1cd89f98872627aa6fba427791fa705e1
AndresMahechaM <elsencillo88@gmail.com>
2021-11-17 23:40:11 -0500

    edicion funcion calcularDepreciacionNIIF

--- Grupo_9/public_html/src.js
+++ Grupo_9/public_html/src.js
@@ -2,12 +2,26 @@ const calcularDepreciacionNIIF = (precioInicial,
                                   precioFinal,
                                   vidaUtil,
                                   numeroPeriodoAconsultar) => {
+    precioDepreciado=0
+    depAgno=precioInicial/vidaUtil
+    if (vidaUtil<=0){
+        return 0;
+        if (numeroPeriodoAconsultar>vidaUtil){
+            precioFinal=precioInicial-(depAgno*vidaUtil)
+            return precioFinal;
+        }else{
+            precioDepreciado=precioInicial-(depAgno*numeroPeriodoAconsultar)
+            return precioDepreciado;
+        }
+    }                                                                
 }
 
 const calcularDepreciacionNIIFEnDolares = (precioInicial,
                                            precioFinal,
                                            vidaUtil,
                                            numeroPeriodoAconsultar) => {
+                                               
+                                               
 }
 
 module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
