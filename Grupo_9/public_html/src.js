5d2581f074738e7c4fb45e07dffd56112bf1d120
HeidyJimenez <90729440+HeidyJimenez@users.noreply.github.com>
2021-11-30 16:56:44 -0600

    sprint 3
    sprint 3

--- Grupo_9/public_html/src.js
+++ Grupo_9/public_html/src.js
@@ -1,28 +1,34 @@
 const calcularDepreciacionNIIF = (precioInicial,
-                                  precioFinal,
-                                  vidaUtil,
-                                  numeroPeriodoAconsultar) => {
-    precioDepreciado=0
-    depAgno=precioInicial/vidaUtil
-    if (vidaUtil<=0){
-        return 0;
-        if (numeroPeriodoAconsultar>vidaUtil){
-            precioFinal=precioInicial-(depAgno*vidaUtil)
-            return precioFinal;
-        }else{
-            precioDepreciado=precioInicial-(depAgno*numeroPeriodoAconsultar)
-            return precioDepreciado;
+    precioFinal,
+    vidaUtil,
+    numeroPeriodoAconsultar) => {
+        if (vidaUtil<=0){
+           precioDepreciado=0;
         }
-    }                                                                
-}
-
+        else if (vidaUtil<numeroPeriodoAconsultar){
+           precioDepreciado=precioFinal;
+        }
+        else{
+            var precioAdepreciar=precioInicial-precioFinal;
+            var depreciacionAnual=precioAdepreciar/vidaUtil;
+            precioDepreciado=precioInicial-(depreciacionAnual*numeroPeriodoAconsultar);}
+        console.log(precioDepreciado);
+        return precioDepreciado;
+    }
+    
 const calcularDepreciacionNIIFEnDolares = (precioInicial,
-                                           precioFinal,
-                                           vidaUtil,
-                                           numeroPeriodoAconsultar) => {
-                                               
-                                               
+        precioFinal,
+        vidaUtil,
+        numeroPeriodoAconsultar) => {
+        if(precioInicial<=0){
+            throw "Error";
+        }
+        else{
+            calcularDepreciacionNIIF(precioInicial,precioFinal,vidaUtil,numeroPeriodoAconsultar);            
+            precioDepreciadoDolares= precioDepreciado/3778;}
+            console.log(precioDepreciadoDolares);
+        return precioDepreciadoDolares;
 }
 
 module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
-module.exports.calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;
+module.exports. calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;
